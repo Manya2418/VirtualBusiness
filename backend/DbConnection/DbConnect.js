a41a205907725cf0ef67e6d58800798b96809ad9
MANYA SAHU <129870190+Manya2418@users.noreply.github.com>
2025-06-17 08:59:45 +0530

    Update DbConnect.js

--- backend/DbConnection/DbConnect.js
+++ backend/DbConnection/DbConnect.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 const dbconnection=async ()=>{
     try{
-        await mongoose.connect("mongodb+srv://manyasahu94:Manya123@cluster0.cbj0e6a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-");
+        await mongoose.connect(process.env.MONGO);
         console.log("Connected");
     }catch(err){
         console.log("Not connected",err);
